d6ce091f2c87faa4f6146791db315df87d4ee116
mur4ik18 <o.o.kotov1999@gmail.com>
2022-12-31 17:18:04 +0100

    save

--- 1/run.c
+++ 1/run.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define int glob = 65;
+
 int main()
 {
   // int
@@ -11,15 +13,19 @@ int main()
   // pop = 0;
   // printf("%d\n", pop);
   
-  char ch = 65;
-  printf("%c%c/%c%c\n", ch, ch+2,ch+3,ch+2);
+  // char ch = 65;
+  // printf("%c%c/%c%c\n", ch, ch+2,ch+3,ch+2);
   
-  double a = 0.0005;
-  printf("%f\n", a);
-  printf("%g\n", 0.0005);
-  printf("%g\n", 0.00005);
-  printf("%e\n", 0.0005);
+  // double a = 0.0005;
+  // printf("%f\n", a);
+  // printf("%g\n", 0.0005);
+  // printf("%g\n", 0.00005);
+  //printf("%e\n", 0.0005);
 
-  
+  // printf("%c\n", glob);
+  printf("%d\n", sizeof(short));
+  printf("%d\n", sizeof(float));
+  printf("%d\n", sizeof(long));
+
   return 0;
 }
