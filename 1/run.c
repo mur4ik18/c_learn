88a2dae2fcb0b5d4135db34e9c33fc0fe09c17e5
mur4ik18 <o.o.kotov1999@gmail.com>
2022-12-31 16:29:16 +0100

    d

--- 1/run.c
+++ 1/run.c
@@ -3,6 +3,23 @@
 
 int main()
 {
-  printf("Hello world!\n");
+  // int
+  // short
+  // long
+  // unsigned == int != -int
+  // unsigned short pop;
+  // pop = 0;
+  // printf("%d\n", pop);
+  
+  char ch = 65;
+  printf("%c%c/%c%c\n", ch, ch+2,ch+3,ch+2);
+  
+  double a = 0.0005;
+  printf("%f\n", a);
+  printf("%g\n", 0.0005);
+  printf("%g\n", 0.00005);
+  printf("%e\n", 0.0005);
+
+  
   return 0;
 }
