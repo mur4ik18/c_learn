6b3f4059a683c7a2cba637efd35c570cf0b8afe2
mur4ik18 <o.o.kotov1999@gmail.com>
2023-01-03 08:47:22 +0100

    new

--- 3/run.c
+++ 3/run.c
@@ -3,8 +3,10 @@
 #include <stdlib.h>
 
 int main() {
-    // malloc();
-    // calloc();
+    // malloc(); выделяет в куче место
+    // malloc(size*sizeof(int))
+    // calloc(); выделяет место в куче и забивает его 0
     // free(); чистит память
+    
     return 0;
 }
