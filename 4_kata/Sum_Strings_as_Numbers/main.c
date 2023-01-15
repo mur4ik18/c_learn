b5aaf3b6f94c96ba5cbb8e31b87618cd0c435058
mur4ik18 <o.o.kotov1999@gmail.com>
2023-01-15 15:36:40 +0100

    :rocket:

---
+++ 4_kata/Sum_Strings_as_Numbers/main.c
@@ -0,0 +1,21 @@
+#include <malloc.h>
+#include <string.h>
+
+
+char *strsum(const char *a, const char *b);
+
+int main() {
+  char* p =strsum("", "");
+  free(p);
+  return 0;
+}
+
+
+
+
+char *strsum(const char *a, const char *b)
+{
+  
+
+  return NULL;
+}
